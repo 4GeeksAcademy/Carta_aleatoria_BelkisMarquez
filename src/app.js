35c32af1a85ba00262416c59613c1332c26f7224
belkisAle <belkismarquezblanco@gmail.com>
2026-02-02 16:44:19 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,24 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = function () {
+
+
+  let suits = ["♦", "♥", "♠", "♣"];
+  let nums = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+  let randomSuits = Math.floor(Math.random() * suits.length);
+  let randomNums = Math.floor(Math.random() * nums.length);
+
+  let cardTwist = document.getElementById("card")
+  document.getElementById("num").innerHTML = nums[randomNums];
+  document.getElementById("icon-superior").innerHTML = suits[randomSuits];
+  document.getElementById("icon-inferior").innerHTML = suits[randomSuits];
+
+  if (suits[randomSuits] == "♦" || suits[randomSuits] == "♥"
+  ) {
+      cardTwist.style.color="red";
+  }
+  
+  // cardTwist.style.color = suits[randomSuits] == "♦" || suits[randomSuits] == "♥" ? "red" : "green";
+
 };
