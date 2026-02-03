070a2c57d057598a49435bc3ba0c42454b68a6fd
belkisAle <belkismarquezblanco@gmail.com>
2026-02-03 21:47:19 +0000

    cambio final

--- src/app.js
+++ src/app.js
@@ -5,24 +5,60 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function () {
+let cardTwist = document.getElementById("card");
 
+let generarCarta = () => {
 
   let suits = ["♦", "♥", "♠", "♣"];
   let nums = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
   let randomSuits = Math.floor(Math.random() * suits.length);
   let randomNums = Math.floor(Math.random() * nums.length);
 
-  let cardTwist = document.getElementById("card")
+
   document.getElementById("num").innerHTML = nums[randomNums];
   document.getElementById("icon-superior").innerHTML = suits[randomSuits];
   document.getElementById("icon-inferior").innerHTML = suits[randomSuits];
 
-  if (suits[randomSuits] == "♦" || suits[randomSuits] == "♥"
+  /* if (suits[randomSuits] == "♦" || suits[randomSuits] == "♥"
   ) {
-      cardTwist.style.color="red";
+    cardTwist.style.color = "red";
+  }
+  else {cardTwist.style.color="black"}; */
+
+  cardTwist.style.color = suits[randomSuits] == "♦" || suits[randomSuits] == "♥" ? "red" : "black";
+}
+let CambioTamanio = () => {
+  let widthCard = document.getElementById("width-card").value;
+  let heigthCard = document.getElementById("heigth-card").value;
+  console.log(widthCard);
+  console.log(heigthCard);
+  cardTwist.style.width = widthCard+ "px"
+  cardTwist.style.height = heigthCard + "px"
+}
+
+window.onload = function () {
+
+  generarCarta();
+
+  let btnChange = document.getElementById("btn-change");
+  btnChange.onclick = () => {
+    generarCarta();
+  }
+
+  let btnSize = document.getElementById("btn-a");
+  btnSize.onclick = () => {
+    console.log('llamada de boton')
+    CambioTamanio()
   }
   
-  // cardTwist.style.color = suits[randomSuits] == "♦" || suits[randomSuits] == "♥" ? "red" : "green";
+  setInterval(() => {
+    generarCarta()},10000);
+  
+
+
+
+
+
+
 
 };
